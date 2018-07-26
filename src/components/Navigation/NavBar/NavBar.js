aa5d172d5d09f2956ab83544dd955fb1340063f7
Ivkaran S. Sawhney <ivesingh@gmail.com>
2018-07-25 20:53:31 -0400

    Fixed Parallax and Navbar, added About EcoSikh

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -1,47 +1,33 @@
 import React from 'react';
-import Logo from '../../Logo/Logo';
+import gurunanak550logo from '../../../assets/logo_sm.png';
 import './NavBar.css';
 
 const navbar = (props) => (
-  <div className="Nav-settings">
-    <nav class="Nav-settings navbar navbar-expand-md fixed-top">
-      <a class="navbar-brand" href="#"><Logo/></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Resources</a>
-          </li>
-        </ul>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign up</button>
-        &nbsp; &nbsp; &nbsp;
-        <button class="btn btn-success my-2 my-sm-0" type="submit">Donate</button>
+      <div className="Nav-settings" >
+        <div className="d-flex flex-column flex-md-row align-items-center p-3  navbar-fixed-top mb-1">
+          <img src={gurunanak550logo} alt="gurunanak550" style={{width: '150px', height: '100px'}}/>
+          <div>
+            <nav className="nav nav-masthead ">
+              <a className="nav-link active" style={{fontSize: '20px'}} href="#">Home</a>
+              <a className="nav-link" style={{fontSize: '20px'}} href="#">About EcoSikh</a>
+              <a className="nav-link" style={{fontSize: '20px'}} href="#">Tree Tutorials</a>
+              <a className="nav-link" style={{fontSize: '20px'}} href="#">Resources</a>
+              <a className="nav-link" style={{fontSize: '20px'}} href="#">Contact Us</a>
+              <a className="nav-link" style={{fontSize: '20px'}} href="#"></a>
+              <div>
+                <a className="btn btn-outline-success" style={{fontSize: '20px', width: '200px'}} href="#">LOG IN</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a className="btn btn-outline-success" style={{fontSize: '20px', width: '200px'}} href="#">DONATE</a>
+              </div>
+              
+            </nav>
+            
+          </div>
+        </div>
       </div>
-    </nav>
-  </div>
-  // <div className="Nav-settings">
-  //   <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 navbar-fixed-top mb-3 box-shadow">
-  //     <h5 className="mr-md-auto"><Logo/></h5>
-  //     <div class="inner">
-  //       <nav class="nav nav-masthead justify-content-center">
-  //         <a className="nav-link active" href="#">About us</a>
-  //         <a className="nav-link" href="#">Resources</a>
-  //         <a className="nav-link" href="#">Support</a>
-  //         <a className="nav-link" href="#">Pricing</a>
-  //       </nav>
-  //     </div>
-  //     &nbsp;&nbsp;
-  //     <a className="btn btn-outline-success" href="#">Sign up</a>
-  //   </div>
-  // </div>
+  
+  
+
 
   // <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   //     <header class="masthead mb-auto">
--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import image from '../../assets/image_1.jpg'
 import './Parallax.css';
-
+import message_2 from '../../assets/message_2.png';
+import message_3 from '../../assets/message_3.png';
 const parallax = () => (
     
     <div>
         <div className="bgimg-1">
-            {/* <div className="caption">
-                <h1 className="border" style={{backgroundColor: 'transparent', fontSize: '70px', color: 'white'}}>
-                    550th Birth Anniversary of Guru Nanak in 2019
-                </h1>
-            </div> */}
+            <div class="flex-container">
+                <div>
+                    <img src={message_2}/>
+                </div>
+                <div>
+                    <img src={message_3}/>
+                </div>
+                <div>
+                    <button className="btn btn-warning btn-lg" 
+                     style={{fontSize: '30px', width: '300px', fontWeight: '900'}}
+                    type="submit">SIGN UP</button>
+                </div>
+            </div>
         </div>
 
-        <div style={{color: '#777', backgroundColor: 'white', textAlign: 'center', padding: '50px 80px', textAlign: 'justify'}}>
-            <h3 style={{textAlign: 'center'}}>Parallax Demo</h3>
-            <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+        <div style={{color: '#777', backgroundColor: '#f6ffb1', alignItems: 'center', padding: '50px 80px', justifyContent: 'center' }}>
+            <h3 style={{textAlign:'center', fontSize: '40px'}}>About EcoSikh</h3>
+            <p style={{fontSize: '30px'}}>EcoSikh is a response from the Sikh community to the threats of climate change and the deterioration of the natural environment. Our organization arose as part of the Long Term Plans for Generational Change programme initiated in 2009 by the United Nations Development Programme (UNDP) and the Alliance of Religions and Conservation, UK (ARC) to help the world’s major religious traditions create long-term plan to improve their relationship with the environment and to guide their investment towards environmental protection.</p>
         </div>
 
         <div className="bgimg-2">
