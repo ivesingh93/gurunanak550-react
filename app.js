effff81e6be06475f64ff661cf1d3dcc6abb150e
Ivkaran Singh <ivesingh@gmail.com>
2018-07-31 19:06:40 -0400

    Setup nodejs server

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
 
-app.get('/', function (req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const port = process.env.PORT || '3000';
+
+app.listen(port, () => {
+    console.log(`App running on localhost:${port}`);
+});
